dd37771001c833efe7fd048c55e2faa4cb3d502c
WEB-AID <web.aid.dev@gmail.com>
2024-05-05 15:53:49 +0200

    architecture creating

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,8 @@
-import webpack from 'webpack';
+import webpack from 'webpack'
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
+
 
 export default (env: BuildEnv) => {
 
@@ -9,17 +10,20 @@ export default (env: BuildEnv) => {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
+        src: path.resolve(__dirname, 'src'),
     }
+
     const mode = env.mode || 'development';
-    const isDev = mode === 'development';
     const PORT = env.port || 3000;
 
+    const isDev = mode === 'development';
+
     const config: webpack.Configuration = buildWebpackConfig({
-        paths,
         mode,
+        paths,
         isDev,
         port: PORT,
     })
 
-    return config;
+    return config
 };
